cb0afecefc986b158d9b428c8093087a51566637
aurelie taschon <aurelie.taschon@gmail.com>
2022-01-04 15:52:42 +0100

    modif BDD

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_04_104715) do
+ActiveRecord::Schema.define(version: 2022_01_04_143148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "classrooms", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_classrooms_on_user_id"
+  end
+
+  create_table "observations", force: :cascade do |t|
+    t.date "obs_date"
+    t.string "category"
+    t.text "note"
+    t.string "obs_color"
+    t.bigint "student_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["student_id"], name: "index_observations_on_student_id"
+    t.index ["user_id"], name: "index_observations_on_user_id"
+  end
+
   create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "classroom"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "classroom_id"
+    t.index ["classroom_id"], name: "index_students_on_classroom_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +59,8 @@ ActiveRecord::Schema.define(version: 2022_01_04_104715) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "classrooms", "users"
+  add_foreign_key "observations", "students"
+  add_foreign_key "observations", "users"
+  add_foreign_key "students", "classrooms"
 end
