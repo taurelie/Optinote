9f7579758f088fdf6f10305a7dc58d50200eef29
aurelie taschon <aurelie.taschon@gmail.com>
2022-01-04 16:23:37 +0100

    before drop

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,6 @@ User.destroy_all
 Classroom.destroy_all
 Student.destroy_all
 
-
 # Seed Users
 puts 'seeds user'
 jdubois = User.create!(email: 'jean.dubois@gmail.com', password: 'jeandubois13', password_confirmation: 'jeandubois13', first_name: 'Jean', last_name: 'Dubois')
