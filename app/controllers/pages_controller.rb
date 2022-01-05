5fa7776f58fd614040c26a667943e5a2f230f1c7
sophie blancard <sblanchard.lw@gmail.com>
2022-01-05 17:23:27 +0100

    form observations _ création bouttons category

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,18 +5,4 @@ class PagesController < ApplicationController
     @user = current_user
   end
 
-  def brouillon
-    @observation = Observation.new # needed to instantiate the form_for
-  end
-
-#   def createb
-#     @observation = Message.new(observation_params) # needed to instantiate the form_for
-#     @observation.save
-#   end
-
-#   private
-
-#   def observation_params
-#     params.require(:observation).permit(:name)
-#   end
 end
--- app/models/observation.rb
+++ app/models/observation.rb
@@ -1,4 +1,6 @@
 class Observation < ApplicationRecord
+  CATEGORY = ["Relation à l'autre", "Expression", "Attitudes corporelles", "Gestes / Actes quotidiens", "Écriture", "Lecture", "Mathématiques", "Comportement", "Environnement", "Autre"]
+  OBS_COLOR = ["green", "grey", "red"]
   belongs_to :student
   belongs_to :user
 end
