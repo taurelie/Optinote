fc9fac4e210fb62a5b5f6bed92d4898c4bf7bbfd
aurelie taschon <aurelie.taschon@gmail.com>
2022-01-07 12:28:05 +0100

    alphabetique + retour icone

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,12 @@ puts 'seeds img avatars'
 img_caroline = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641471950/WAGON/Caroline_hveiv2.jpg')
 img_florian = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641471951/WAGON/Florian_kp96z3.jpg')
 img_carlos = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641471950/WAGON/Carlos_vaymrg.jpg')
-img_elise = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641471950/WAGON/Elise_o8p4iw.jpg')
+img_isabelle = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641552467/WAGON/Isabelle_s98zgg.jpg')
 img_gladys = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641471951/WAGON/Gladys_ok8dyy.jpg')
 img_nathan = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641471951/WAGON/Nathan_njvrdl.jpg')
 img_maxime = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641471951/WAGON/Maxime_y0bt3h.jpg')
 img_maya = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641471951/WAGON/Maya_fmkpdz.jpg')
-img_sarah = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641471951/WAGON/Sarah_pphfpc.jpg')
+img_sarah = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641552467/WAGON/Sarah_ms6exd.jpg')
 img_claire = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641471950/WAGON/Claire_cfvyhw.jpg')
 img_irina = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641471951/WAGON/Irina_aoomaw.jpg')
 img_karine = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641471951/WAGON/Karine_qwqyll.jpg')
@@ -27,10 +27,10 @@ img_fatia = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641471
 img_timeo = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641471952/WAGON/Tim%C3%A9o_qfa1b0.jpg')
 
 puts 'seeds user'
-jdubois = User.create!(email: 'jean.dubois@gmail.com', password: 'jeandubois13', password_confirmation: 'jeandubois13', first_name: 'Jean', last_name: 'Dubois')
+gjing = User.create!(email: 'guole.jing@gmail.com', password: '123456789', password_confirmation: '123456789', first_name: 'Guole', last_name: 'Jing')
 
 puts 'seeds classroom'
-ce1 = Classroom.create!(name: 'CE1', user: jdubois)
+ce1 = Classroom.create!(name: 'CE1', user: gjing)
 
 puts 'seed student'
 cteissier = Student.create!(first_name: 'Caroline', last_name: 'Tessier', classroom: ce1)
@@ -42,8 +42,8 @@ fbernard.photo.attach(io: img_florian, filename: 'Florian_kp96z3.jpg', content_t
 cdasilva = Student.create!(first_name: 'Carlos', last_name: 'Da Silva', classroom: ce1)
 cdasilva.photo.attach(io: img_carlos, filename: 'Carlos_vaymrg.jpg', content_type: 'image/jpg')
 
-epetit = Student.create!(first_name: 'Elise', last_name: 'Petit', classroom: ce1)
-epetit.photo.attach(io: img_elise, filename: 'Elise_o8p4iw.jpg', content_type: 'image/jpg')
+epetit = Student.create!(first_name: 'Isabelle', last_name: 'Petit', classroom: ce1)
+epetit.photo.attach(io: img_isabelle, filename: 'Isabelle_s98zgg', content_type: 'image/jpg')
 
 grobert = Student.create!(first_name: 'Gladys', last_name: 'Robert', classroom: ce1)
 grobert.photo.attach(io: img_gladys, filename: 'Gladys_ok8dyy.jpg', content_type: 'image/jpg')
@@ -58,7 +58,7 @@ mlopez = Student.create!(first_name: 'Maya', last_name: 'Lopez', classroom: ce1)
 mlopez.photo.attach(io: img_maya, filename: 'Maya_fmkpdz.jpg', content_type: 'image/jpg')
 
 slamy = Student.create!(first_name: 'Sarah', last_name: 'Lamy', classroom: ce1)
-slamy.photo.attach(io: img_sarah, filename: 'Sarah_pphfpc.jpg', content_type: 'image/jpg')
+slamy.photo.attach(io: img_sarah, filename: 'Sarah_ms6exd.jpg', content_type: 'image/jpg')
 
 cvidal = Student.create!(first_name: 'Claire', last_name: 'Vidal', classroom: ce1)
 cvidal.photo.attach(io: img_claire, filename: 'Claire_cfvyhw.jpg', content_type: 'image/jpg')
@@ -94,26 +94,30 @@ tdeccicio = Student.create!(first_name: 'Timéo', last_name: 'Deccicio', classro
 tdeccicio.photo.attach(io: img_timeo, filename: 'Timéo_qfa1b0.jpg', content_type: 'image/jpg')
 
 puts 'seed observations'
-relation1 = Observation.create!(obs_date: 20210920, category: "Relation à l'autre", note: "Pleure mis en groupe avec Lucas", obs_color: "Red", student: chumbert, user: jdubois)
-relation2 = Observation.create!(obs_date: 20210928, category: "Relation à l'autre", note: "Attend plusieurs minutes poru sortir de la classe", obs_color: "Grey", student: chumbert, user: jdubois)
-relation3 = Observation.create!(obs_date: 20210928, category: "Relation à l'autre", note: "Est en conflit avec les autres", obs_color: "Red", student: lrodriguez, user: jdubois)
-relation4 = Observation.create!(obs_date: 20211011, category: "Relation à l'autre", note: "Est réfractaire à l'apprentissage, refuse d'apprendre", obs_color: "Red", student: lrodriguez, user: jdubois)
-relation5 = Observation.create!(obs_date: 20211012, category: "Relation à l'autre", note: "A envie de participer mais n'ose pas (lève la main puis ne participe pas)", obs_color: "Grey", student: slamy, user: jdubois)
-relation6 = Observation.create!(obs_date: 20211129, category: "Relation à l'autre", note: "Ne participe pas, mais suit en classe", obs_color: "Green", student: slamy, user: jdubois)
-expression1 = Observation.create!(obs_date: 20211102, category: "Expression", note: "Ne réagit pas aux injonctions, aux demandes orales", obs_color: "Red", student: lrodriguez, user: jdubois)
-expression2 = Observation.create!(obs_date: 20211102, category: "Expression", note: "Parle à voix basse, est peu audible", obs_color: "Grey", student: slamy, user: jdubois)
-expression3 = Observation.create!(obs_date: 20211025, category: "Expression", note: "Restituer oralement une leçon", obs_color: "Green", student: ieurtein, user: jdubois)
-attitude1 = Observation.create!(obs_date: 20211215, category: "Attitudes corporelles", note: "A une grande fatigabilité", obs_color: "Grey", student: slamy, user: jdubois)
-gestesquotidiens1 = Observation.create!(obs_date: 20211207, category: "Gestes / actes quotidiens", note: "Se repère dans l'espace avec difficulté (tableau, graphique, cahier, dictionnaire...)", obs_color: "Red", student: slamy, user: jdubois)
-ecriture1 = Observation.create!(obs_date: 20211011, category: "Ecriture", note: "Ne fixe pas l'orthographe des mots", obs_color: "Red", student: gmakalu, user: jdubois)
-lecture1 = Observation.create!(obs_date: 20211215, category: "Lecture", note: "Ne comprend pas l'implicite, le second degrés, le sens figuré...", obs_color: "Red", student: lrodriguez, user: jdubois)
-math1 = Observation.create!(obs_date: 20211102, category: "Mathématiques", note: "Accède difficilement au calcul mental", obs_color: "Grey", student: slamy, user: jdubois)
-comportement1 = Observation.create!(obs_date: 20211215, category: "Comportement", note: "A un principe de réalité erroné (la récréation dure deux semaines)", obs_color: "Grey", student: lrodriguez, user: jdubois)
-environnement1 = Observation.create!(obs_date: 20211011, category: "Environnement", note: "Devoirs non-faits", obs_color: "Red", student: lrodriguez, user: jdubois)
-autre1 = Observation.create!(obs_date: 20211215, category: "Autre", note: "Parents ne viennent pas aux convocations", obs_color: "Red", student: lrodriguez, user: jdubois)
+relation1 = Observation.create!(obs_date: 20210920, category: "Relation à l'autre", note: "Pleure mis en groupe avec Lucas", obs_color: "Red", student: chumbert, user: gjing)
+relation2 = Observation.create!(obs_date: 20210928, category: "Relation à l'autre", note: "Attend plusieurs minutes poru sortir de la classe", obs_color: "Grey", student: chumbert, user: gjing)
+relation3 = Observation.create!(obs_date: 20210928, category: "Relation à l'autre", note: "Est en conflit avec les autres", obs_color: "Red", student: lrodriguez, user: gjing)
+relation4 = Observation.create!(obs_date: 20211011, category: "Relation à l'autre", note: "Est réfractaire à l'apprentissage, refuse d'apprendre", obs_color: "Red", student: lrodriguez, user: gjing)
+relation5 = Observation.create!(obs_date: 20211012, category: "Relation à l'autre", note: "A envie de participer mais n'ose pas (lève la main puis ne participe pas)", obs_color: "Grey", student: cteissier, user: gjing)
+relation6 = Observation.create!(obs_date: 20211129, category: "Relation à l'autre", note: "Ne participe pas, mais suit en classe", obs_color: "Green", student: cteissier, user: gjing)
+expression1 = Observation.create!(obs_date: 20211102, category: "Expression", note: "Ne réagit pas aux injonctions, aux demandes orales", obs_color: "Red", student: lrodriguez, user: gjing)
+expression2 = Observation.create!(obs_date: 20211102, category: "Expression", note: "Parle à voix basse, est peu audible", obs_color: "Grey", student: cteissier, user: gjing)
+expression3 = Observation.create!(obs_date: 20211025, category: "Expression", note: "Restituer oralement une leçon", obs_color: "Green", student: ieurtein, user: gjing)
+attitude1 = Observation.create!(obs_date: 20211215, category: "Attitudes corporelles", note: "A une grande fatigabilité", obs_color: "Grey", student: cteissier, user: gjing)
+gestesquotidiens1 = Observation.create!(obs_date: 20211207, category: "Gestes / actes quotidiens", note: "Se repère dans l'espace avec difficulté (tableau, graphique, cahier, dictionnaire...)", obs_color: "Red", student: cteissier, user: gjing)
+ecriture1 = Observation.create!(obs_date: 20211011, category: "Ecriture", note: "Ne fixe pas l'orthographe des mots", obs_color: "Red", student: gmakalu, user: gjing)
+lecture1 = Observation.create!(obs_date: 20211215, category: "Lecture", note: "Ne comprend pas l'implicite, le second degrés, le sens figuré...", obs_color: "Red", student: lrodriguez, user: gjing)
+math1 = Observation.create!(obs_date: 20211102, category: "Mathématiques", note: "Accède difficilement au calcul mental", obs_color: "Grey", student: cteissier, user: gjing)
+comportement1 = Observation.create!(obs_date: 20211215, category: "Comportement", note: "A un principe de réalité erroné (la récréation dure deux semaines)", obs_color: "Grey", student: lrodriguez, user: gjing)
+environnement1 = Observation.create!(obs_date: 20211011, category: "Environnement", note: "Devoirs non-faits", obs_color: "Red", student: lrodriguez, user: gjing)
+autre1 = Observation.create!(obs_date: 20210921, category: "Autre", note: "Parents ne viennent pas aux convocations", obs_color: "Red", student: lrodriguez, user: gjing)
+retard1 = Observation.create!(obs_date: 20211011, category: "Retard", note: "20mn", obs_color: "Red", student: slamy, user: gjing)
+retard1 = Observation.create!(obs_date: 20211129, category: "Retard", note: "15mn", obs_color: "Red", student: slamy, user: gjing)
+retard1 = Observation.create!(obs_date: 20211210, category: "Retard", note: "20mn", obs_color: "Red", student: slamy, user: gjing)
+retard1 = Observation.create!(obs_date: 20211215, category: "Retard", note: "25mn", obs_color: "Red", student: slamy, user: gjing)
 
 puts 'seeds img'
-img_add_note = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641390506/notes_qaexuf.png')
+img_add_note = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641554151/noteadd_t3diri.png')
 img_note_avatar = URI.open('https://res.cloudinary.com/dowgjinna/image/upload/v1641478873/notes_pile_sngwow.png')
 
 puts 'done'
