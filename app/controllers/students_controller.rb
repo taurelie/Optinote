6556a088eda95e49618a1a4fc1b3cc7f37084d74
sophie blancard <sblanchard.lw@gmail.com>
2022-01-10 13:07:23 +0100

    show student date ok with date form

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,9 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
     @observation = Observation.where(student_id: @student.id)
+    @observations = @student.observations.sort do |obs_a, obs_b|
+      obs_b.creation_date <=> obs_a.creation_date
+    end
   end
 
   def index
